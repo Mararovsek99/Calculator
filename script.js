c6e6681c56a6d4c6639fc0d36963031c8cf388b2
Mararovsek99 <andrej.marovsek99@gmail.com>
2024-03-16 14:14:14 +0100

    edited html and css, its iphone look alike calculator

--- script.js
+++ script.js
@@ -14,7 +14,28 @@ const divide = function(a, b) {
     return a/b;
      }   
 
+let firstNum;
+let secondNum;
+let operator;
 
-
-
+const operate = function(a,b){
+    if (operator === "+"){
+       return add(a, b);
+    }
+    else if (operator === "-"){
+        return subtract(a, b);
+    }
+    else if (operator === "*"){
+        return multiply(a, b);
+    }
+    else if (operator === "/"){
+        if(b !== 0){
+            return divide(a, b);
+        }
+        else{
+            return "cannot divide by zero!"
+        }
+        
+    }
+}
  
